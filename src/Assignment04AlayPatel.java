656b00952941f51410c99afc5546f38325745dc5
Alay Patel <patelalaykumar2@gmail.com>
2020-12-07 21:02:49 -0500

    Corrected the problem with the loop.
    The condition was permitting the loop to run too many times.
    
    Signed-off-by: Alay Patel <patelalaykumar2@gmail.com>

--- src/Assignment04AlayPatel.java
+++ src/Assignment04AlayPatel.java
@@ -7,7 +7,7 @@ public class Assignment04AlayPatel {
 		int[] primes = { 2, 3, 5, 7 };
 
 		// loop to print all integers stored in the primes array
-		for (int index = 0; index <= primes.length; index++) {
+		for (int index = 0; index < primes.length; index++) {
 			System.out.println(primes[index]);
 		}
 
