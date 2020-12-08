d7e3dc9177a09883cdbb2ae22fa173c0593efc96
Alay Patel <patelalaykumar2@gmail.com>
2020-12-07 21:00:23 -0500

    Added programmer comments, and starter code to method main.
    Signed-off-by: Alay Patel <patelalaykumar2@gmail.com>

--- src/Assignment04AlayPatel.java
+++ src/Assignment04AlayPatel.java
@@ -2,6 +2,17 @@
 public class Assignment04AlayPatel {
 
 	public static void main(String[] args) {
+
+		// array of integers containing prime numbers
+		int[] primes = { 2, 3, 5, 7 };
+
+		// loop to print all integers stored in the primes array
+		for (int index = 0; index <= primes.length; index++) {
+			System.out.println(primes[index]);
+		}
+
+		// author information
+		System.out.println("Program by Alay Patel");
 	}
 
 }
